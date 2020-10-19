8c73a35db07e6807cf8e111f81dca19cea430bc4
chesserm <chesserm@umich.edu>
2020-10-19 10:14:05 -0400

    Extended Bootstrap Framework with Sprite and Text
    Added Sprite and Textbox objects which inherit from Component and can be placed using the grid-based framework I made in Screen.cs (and Row.cs). The Sprite class is any image we want to add (that doesn't need click functionality like a button). The Textbox class is for adding text to the screen based on the grid system (so pixel math doesn't have to be used).

---
+++ testing_v2/Controls/Sprite.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testing_v2.Controls
+{
+    class Sprite : Component
+    {
+        #region MemberVariables
+
+        private Rectangle _rectangle = new Rectangle(0, 0, 100, 100);
+        private Vector2 _position = new Vector2(0, 0);
+
+        #endregion
+
+        #region Properties
+        public Vector2 Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public override Rectangle Rectangle
+        {
+            get
+            {
+                return _rectangle;
+            }
+            set
+            {
+                _rectangle = value;
+                _position = new Vector2(_rectangle.X, _rectangle.Y);
+            }
+        }
+
+        #endregion
+
+        
+        public Sprite(Texture2D texture)
+        {
+            // Save texture in the inherited texture value from Component
+            ComponentTexture = texture;
+        }
+
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // You can change the color of this to Gray if you want to darken the shading
+            spriteBatch.Draw(ComponentTexture, Rectangle, Color.White);
+            
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Do nothing (this function needs to be overrided bc it inherits from Component)
+            // In the future we can add animations here
+            return;
+        }
+    }
+}
---
+++ testing_v2/Controls/Textbox.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace testing_v2.Controls
+{
+    class Textbox : Component
+    {
+
+        #region MemberVariables
+        
+        private SpriteFont _font;
+
+        private Rectangle _rectangle = new Rectangle(0, 0, 100, 100);
+        private Vector2 _position = new Vector2(0, 0);
+
+
+        #endregion
+
+        #region Properties
+        public Vector2 Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public override Rectangle Rectangle
+        {
+            get
+            {
+                return _rectangle;
+            }
+            set
+            {
+                _rectangle = value;
+                _position = new Vector2(_rectangle.X, _rectangle.Y);
+            }
+        }
+
+        public string Text { get; set; }
+
+        public Color TextColor { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public Textbox(SpriteFont spriteFont, String text)
+        {
+            _font = spriteFont;
+            Text = text;
+            TextColor = Color.Black;
+        }
+
+        public Textbox(SpriteFont spriteFont, String text, Color textColor)
+        {
+            _font = spriteFont;
+            Text = text;
+            TextColor = textColor;
+        }
+
+        #endregion
+
+        #region Functions
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            // This if statement was directly taken from the tutorial referenced 
+            // at the top of Button and seems to be the standard way of doing this 
+            if (!string.IsNullOrEmpty(Text))
+            {
+                var x = (Rectangle.X + (Rectangle.Width / 2)) - (_font.MeasureString(Text).X / 2);
+                var y = (Rectangle.Y + (Rectangle.Height / 2)) - (_font.MeasureString(Text).Y / 2);
+
+
+                spriteBatch.DrawString(_font, Text, new Vector2(x, y), TextColor);
+            }
+            return;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Update not needed for textboxes, but function had to be overridden from Component
+            return;
+        }
+
+        #endregion
+    }
+}
